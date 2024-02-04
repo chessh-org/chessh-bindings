53631ffea7a7241bc3f971f2617911f72d867129
Nate Choe <nate@natechoe.dev>
2024-02-04 16:14:34 -0600

    get_move

--- src/chessh.c
+++ src/chessh.c
@@ -11,9 +11,27 @@
 
 #include "chessh.h"
 
+#define LOGIN 0x00
+#define MAKE_MOVE 0x01
+#define GET_BOARD 0x02
+#define GET_VALID_MOVES 0x03
+#define INIT_GAME 0x04
+#define BOARD_INFO 0x05
+#define MOVE_INFO 0x06
+#define NOTIFY 0x07
+
+#define draw_offer 0x00
+#define white_wins 0x01
+#define black_wins 0x02
+#define forced_draw 0x03
+#define internal_server_error 0x04
+#define your_turn 0x05
+#define illegal_move 0x06
+#define needs_promotion 0x07
+
 static int create_socket(char const * const host, int const port);
 static int send_string(CHESSH const * const endpoint, char const * const string);
-static int write_resilient(int const fd, void const * const buff, size_t const len);
+static int get_move(CHESSH const * const endpoint, chessh_move *ret);
 
 CHESSH *chessh_connect(char const * const host, int const port,
 		char const * const user, char const * const pass) {
@@ -24,13 +42,20 @@ CHESSH *chessh_connect(char const * const host, int const port,
 	if ((ret->fd = create_socket(host, port)) < 0) {
 		goto error2;
 	}
+	if ((ret->file = fdopen(ret->fd, "r+")) == NULL) {
+		goto error3;
+	}
 
 	if (send_string(ret, user) < 0 ||
 	    send_string(ret, pass)) {
-		goto error2;
+		goto error4;
 	}
 
 	return ret;
+error4:
+	fclose(ret->file);
+error3:
+	close(ret->fd);
 error2:
 	free(ret);
 error1:
@@ -40,6 +65,23 @@ error1:
 void chessh_disconnect(CHESSH *connection) {
 	close(connection->fd);
 	free(connection);
+}
+
+int chessh_wait(CHESSH *connection, chessh_event * const event) {
+	int type;
+	type = fgetc(connection->file);
+	if (type == EOF) {
+		return -1;
+	}
+	switch (type) {
+	/* None of these commands should ever get sent TO the client */
+	case LOGIN: case GET_BOARD: case GET_VALID_MOVES: default:
+		return -1;
+	case MAKE_MOVE:
+		event->type = CHESSH_EVENT_MOVE;
+		return get_move(connection, &event->move.move);
+	}
+	return 0;
 }
 
 /*! @brief Creates a new client socket
@@ -80,7 +122,7 @@ connected:
 
 /*! @brief Sends a string through a CHESSH * endpoint
  *
- * @param endpoint THe CHESSH * endpoint to send the string through
+ * @param endpoint The CHESSH * endpoint to send the string through
  * @param string The string to send
  */
 static int send_string(CHESSH const * const endpoint, char const * const string) {
@@ -92,42 +134,26 @@ static int send_string(CHESSH const * const endpoint, char const * const string)
 	}
 	buff[0] = len;
 	memcpy(buff+1, string, len);
-	return write_resilient(endpoint->fd, (void *) buff, len+1);
+	return fwrite((void *) buff, len+1, 1, endpoint->file) < 1 ? -1:0;
 }
 
-/*! @brief Writes a buffer to a file, chaining multiple `write` calls if
- * necessary
+/*! @brief Parses a move from an endpoint
  *
- * @param fd The file to send to
- * @param buff The buffer to write
- * @param len The size of the buffer
+ * @param endpoint The endpoint to get a move from
+ * @param ret The return location of the move
  */
-static int write_resilient(int const fd, void const * const buff, size_t const len) {
-	size_t progress = 0;
-	char *cbuff = (char *) buff;
-
-	while (progress < len) {
-		ssize_t dp;
-		dp = write(fd, cbuff + progress, len - progress);
-		if (dp < 0) {
-			if (errno == EINTR) {
-				continue;
-			}
-			/* TODO: Slowloris prevention? */
-			if (errno == EAGAIN) {
-				struct pollfd pollfd;
-				pollfd.fd = fd;
-				pollfd.events = POLLOUT;
-				poll(&pollfd, 1, 3000);
-				if (!(pollfd.revents & POLLOUT)) {
-					return -1;
-				}
-				continue;
-			}
-			return -1;
-		}
-		progress += dp;
+static int get_move(CHESSH const * const endpoint, chessh_move *ret) {
+	int c1 = fgetc(endpoint->file);
+	int c2 = fgetc(endpoint->file);
+	if (c1 == EOF || c2 == EOF || c1 > 0xff || c2 > 0xff) {
+		return -1;
 	}
-
+	ret->r_i = (c1 >> 5) & 7;
+	ret->c_i = (c1 >> 2) & 7;
+	ret->has_promotion = c1 & 2;
+	ret->padding = 0;
+	ret->r_f = (c2 >> 5) & 7;
+	ret->c_f = (c2 >> 2) & 7;
+	ret->promotion = c2 & 3;
 	return 0;
 }
--- src/chessh.h
+++ src/chessh.h
@@ -1,26 +1,32 @@
 #ifndef HAVE_CHESSH_BINDINGS
 #define HAVE_CHESSH_BINDINGS
 
+#include <stdio.h>
+#include <stdint.h>
+
 typedef enum {
-	chessh_rook,
-	chessh_knight,
-	chessh_bishop,
-	chessh_queen,
-	chessh_king,
-	chessh_pawn,
-	chessh_empty,
+	CHESSH_ROOK,
+	CHESSH_KNIGHT,
+	CHESSH_BISHOP,
+	CHESSH_QUEEN,
+	CHESSH_KING,
+	CHESSH_PAWN,
+	CHESSH_EMPTY,
 } chessh_piece_type;
 
 typedef enum {
-	chessh_white,
-	chessh_black
+	CHESSH_WHITE,
+	CHESSH_BLACK
 } chessh_player;
 
 typedef struct {
-	int r_i;
-	int c_i;
-	int r_f;
-	int c_f;
+	uint16_t r_i : 3;
+	uint16_t c_i : 3;
+	uint16_t has_promotion : 1;
+	uint16_t padding : 1;
+	uint16_t r_f : 3;
+	uint16_t c_f : 3;
+	uint16_t promotion : 2;
 } chessh_move;
 
 typedef struct {
@@ -30,8 +36,31 @@ typedef struct {
 
 typedef chessh_piece chessh_board[8][8];
 
+typedef enum {
+	CHESSH_EVENT_MOVE,
+	CHESSH_EVENT_FOUND_OP,
+	CHESSH_EVENT_UNKNOWN
+} chessh_event_type;
+
+typedef struct {
+	chessh_event_type type; /* always CHESSH_EVENT_MOVE */
+	chessh_move move;
+} chessh_event_move;
+
+typedef struct {
+	chessh_event_type type; /* Always CHESSH_EVENT_FOUND_OP */
+	chessh_player player;   /* Set to the player you're playing as, not against. */
+} chessh_event_found_op;
+
+typedef union {
+	chessh_event_type type;
+	chessh_event_move move;
+	chessh_event_found_op found_op;
+} chessh_event;
+
 typedef struct {
 	int fd;
+	FILE *file;
 	chessh_player player;
 } CHESSH;
 
@@ -53,5 +82,13 @@ CHESSH *chessh_connect(char const * const host, int const port,
  * @see chessh_connect
  */
 void chessh_disconnect(CHESSH *connection);
+
+/*! @brief Waits for an event from the server
+ *
+ * @param connection The connection to wait for an event from
+ * @param event The return location of the event
+ * @return 0 on success, -1 on error
+ */
+int chessh_wait(CHESSH *connection, chessh_event * const event);
 
 #endif
