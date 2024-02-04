42ddfc8b2fc8fa056b56fb9d33c6770951e4bdb4
Nate Choe <nate@natechoe.dev>
2024-02-04 15:39:06 -0600

    Logging in

--- src/chessh.c
+++ src/chessh.c
@@ -1,6 +1,9 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#include <poll.h>
 #include <netdb.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -8,18 +11,30 @@
 
 #include "chessh.h"
 
-static int create_socket(char *host, int port);
+static int create_socket(char const * const host, int const port);
+static int send_string(CHESSH const * const endpoint, char const * const string);
+static int write_resilient(int const fd, void const * const buff, size_t const len);
 
-CHESSH *chessh_connect(char *host, int port) {
+CHESSH *chessh_connect(char const * const host, int const port,
+		char const * const user, char const * const pass) {
 	CHESSH *ret;
 	if ((ret = malloc(sizeof *ret)) == NULL) {
-		return NULL;
+		goto error1;
 	}
 	if ((ret->fd = create_socket(host, port)) < 0) {
-		free(ret);
-		return NULL;
+		goto error2;
 	}
+
+	if (send_string(ret, user) < 0 ||
+	    send_string(ret, pass)) {
+		goto error2;
+	}
+
 	return ret;
+error2:
+	free(ret);
+error1:
+	return NULL;
 }
 
 void chessh_disconnect(CHESSH *connection) {
@@ -27,12 +42,12 @@ void chessh_disconnect(CHESSH *connection) {
 	free(connection);
 }
 
-/*! \brief Creates a new client socket
+/*! @brief Creates a new client socket
  *
- * \param host The hostname of the server to connect to
- * \param port THe port of the server to connect to
+ * @param host The hostname of the server to connect to
+ * @param port THe port of the server to connect to
  */
-static int create_socket(char *host, int port) {
+static int create_socket(char const * const host, int const port) {
 	char portstr[20];
 	struct addrinfo hints;
 	struct addrinfo *addr;
@@ -61,4 +76,58 @@ static int create_socket(char *host, int port) {
 connected:
 	freeaddrinfo(addr);
 	return sockfd;
+}
+
+/*! @brief Sends a string through a CHESSH * endpoint
+ *
+ * @param endpoint THe CHESSH * endpoint to send the string through
+ * @param string The string to send
+ */
+static int send_string(CHESSH const * const endpoint, char const * const string) {
+	size_t len;
+	char buff[257];
+	len = strlen(string);
+	if (len >= 256) {
+		return -1;
+	}
+	buff[0] = len;
+	memcpy(buff+1, string, len);
+	return write_resilient(endpoint->fd, (void *) buff, len+1);
+}
+
+/*! @brief Writes a buffer to a file, chaining multiple `write` calls if
+ * necessary
+ *
+ * @param fd The file to send to
+ * @param buff The buffer to write
+ * @param len The size of the buffer
+ */
+static int write_resilient(int const fd, void const * const buff, size_t const len) {
+	size_t progress = 0;
+	char *cbuff = (char *) buff;
+
+	while (progress < len) {
+		ssize_t dp;
+		dp = write(fd, cbuff + progress, len - progress);
+		if (dp < 0) {
+			if (errno == EINTR) {
+				continue;
+			}
+			/* TODO: Slowloris prevention? */
+			if (errno == EAGAIN) {
+				struct pollfd pollfd;
+				pollfd.fd = fd;
+				pollfd.events = POLLOUT;
+				poll(&pollfd, 1, 3000);
+				if (!(pollfd.revents & POLLOUT)) {
+					return -1;
+				}
+				continue;
+			}
+			return -1;
+		}
+		progress += dp;
+	}
+
+	return 0;
 }
--- src/chessh.h
+++ src/chessh.h
@@ -1,24 +1,57 @@
 #ifndef HAVE_CHESSH_BINDINGS
 #define HAVE_CHESSH_BINDINGS
 
+typedef enum {
+	chessh_rook,
+	chessh_knight,
+	chessh_bishop,
+	chessh_queen,
+	chessh_king,
+	chessh_pawn,
+	chessh_empty,
+} chessh_piece_type;
+
+typedef enum {
+	chessh_white,
+	chessh_black
+} chessh_player;
+
+typedef struct {
+	int r_i;
+	int c_i;
+	int r_f;
+	int c_f;
+} chessh_move;
+
+typedef struct {
+	chessh_player player : 1;
+	chessh_piece_type piece : 3;
+} chessh_piece;
+
+typedef chessh_piece chessh_board[8][8];
+
 typedef struct {
 	int fd;
+	chessh_player player;
 } CHESSH;
 
-/*! \brief Connects to a chessh API endpoint
+/*! @brief Connects to a chessh API endpoint
  * 
- * \param host The hostname of the API endpoint
- * \param port The port of the API endpoint
- * \see chessh_disconnect
- * \return The chessh connection object
+ * @param host The hostname of the API endpoint
+ * @param port The port of the API endpoint
+ * @param user The user to log in as
+ * @param pass The password of said user
+ * @see chessh_disconnect
+ * @return The chessh connection object
  */
-extern CHESSH *chessh_connect(char *host, int port);
+CHESSH *chessh_connect(char const * const host, int const port,
+		char const * const user, char const * const pass);
 
-/*! \brief Disconnects from and frees a chessh API endpoint
+/*! @brief Disconnects from and frees a chessh API endpoint
  *
- * \param connection The connection to close
- * \see chessh_connect
+ * @param connection The connection to close
+ * @see chessh_connect
  */
-extern void chessh_disconnect(CHESSH *connection);
+void chessh_disconnect(CHESSH *connection);
 
 #endif
